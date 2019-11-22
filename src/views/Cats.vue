9ff4efed1ec703c7241e12db6f7cfab15aadac66
Shane O'Moore <Shane.OMoore@livetiles.nyc>
2019-11-22 12:10:50 +0000

    Wire up data

--- src/views/Cats.vue
+++ src/views/Cats.vue
@@ -1,3 +1,18 @@
 <template>
-  <div>Cats</div>
+  <div>
+    <h1>Cats for adoption</h1>
+    <b-table striped hover :items="cats"></b-table>
+  </div>
 </template>
+
+<script>
+import cats from '@/data/cats'
+
+export default {
+  data () {
+    return {
+      cats
+    }
+  }
+}
+</script>
--- src/views/Dogs.vue
+++ src/views/Dogs.vue
@@ -1,3 +1,18 @@
 <template>
-  <div>Dogs</div>
+  <div>
+    <h1>Dogs for adoption</h1>
+    <b-table striped hover :items="dogs"></b-table>
+  </div>
 </template>
+
+<script>
+import dogs from '@/data/dogs'
+
+export default {
+  data () {
+    return {
+      dogs
+    }
+  }
+}
+</script>
